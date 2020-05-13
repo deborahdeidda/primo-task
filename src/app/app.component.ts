c39752f81c44f126e39b6fcd3f751fa4d9ddbb48
Deborah Deidda <deborahdeidda@outlook.it>
2020-05-13 10:57:23 +0200

    Primo task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'primo-task';
+  title = 'Primo task - Button';
+
+  btnConfig = {
+    styles: {
+      position: 'relative',
+      width: '100px',
+      height: '40px',
+      backgroundColor: '#eda6c0',
+      color: 'black',
+      fontSize: '15px'
+    },
+    text: 'Button'
+  };
+
 }
